622cc50018a34c1984cec9a0933119a4019a9bbb
Laha Luhem <ahalmehul+github@gmail.com>
2025-04-11 18:11:31 +0200

    build runner run post gen

--- flutter_gen/hooks/post_gen.dart
+++ flutter_gen/hooks/post_gen.dart
@@ -2,15 +2,28 @@ import 'dart:io';
 import 'package:mason/mason.dart';
 
 Future<void> run(HookContext context) async {
-  final progress = context.logger.progress('Installing flutter_svg dependency');
+  final depsProgress =
+      context.logger.progress('Installing flutter_svg dependency');
 
-  final result = await Process.run('dart', ['pub', 'add', 'flutter_svg']);
-  context.logger.info(result.stdout);
-  if ((result.stderr as String).isNotEmpty) {
-    context.logger.err(result.stderr);
-    progress.fail('Failed to install slang and slang_flutter dependencies');
+  final depsResult = await Process.run('dart', ['pub', 'add', 'flutter_svg']);
+  context.logger.info(depsResult.stdout);
+  if ((depsResult.stderr as String).isNotEmpty) {
+    context.logger.err(depsResult.stderr);
+    depsProgress.fail('Failed to install slang and slang_flutter dependencies');
     return;
   }
 
-  progress.complete('Installed flutter_svg dependency');
+  depsProgress.complete('Installed flutter_svg dependency');
+
+  final buildRunnerProgress = context.logger.progress('Generating assets');
+  final buildRunneResult = await Process.run(
+      'dart', ['run', 'build_runner', 'build', '--delete-conflicting-outputs']);
+  context.logger.info(buildRunneResult.stdout);
+  if ((buildRunneResult.stderr as String).isNotEmpty) {
+    context.logger.err(buildRunneResult.stderr);
+    buildRunnerProgress.fail('Failed to run build_runner generate command');
+    return;
+  }
+
+  buildRunnerProgress.complete('Generated assets');
 }
