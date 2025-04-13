4565bc1c360968defd77368d97c58c13ae274ea5
Laha Luhem <ahalmehul+github@gmail.com>
2025-04-13 17:22:10 +0200

    Use injectable for DI

---
+++ mvvm_app_template/__brick__/{{app_name}}/lib/locator.config.dart
@@ -0,0 +1,43 @@
+// dart format width=100
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:flutter_secure_storage/flutter_secure_storage.dart' as _i558;
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
+
+import 'features/auth/services/auth_service.dart' as _i620;
+import 'features/core/services/hive/hive_storage_service.dart' as _i511;
+import 'features/core/services/logging_service.dart' as _i969;
+import 'features/core/services/storage_service.dart' as _i568;
+import 'features/core/views/home/home_view_model.dart' as _i104;
+
+extension GetItInjectableX on _i174.GetIt {
+  // initializes the registration of main-scope dependencies inside of GetIt
+  _i174.GetIt init({String? environment, _i526.EnvironmentFilter? environmentFilter}) {
+    final gh = _i526.GetItHelper(this, environment, environmentFilter);
+    final registerFlutterSecureStorageModule = _$RegisterFlutterSecureStorageModule();
+    gh.factory<_i969.LoggingService>(() => _i969.LoggingService());
+    gh.singleton<_i558.FlutterSecureStorage>(
+      () => registerFlutterSecureStorageModule.flutterSecureStorage,
+    );
+    gh.lazySingleton<_i620.AuthService>(() => const _i620.AuthService());
+    gh.singleton<_i568.StorageService>(
+      () => _i511.HiveStorageService(flutterSecureStorage: gh<_i558.FlutterSecureStorage>()),
+      signalsReady: true,
+    );
+    gh.factory<_i104.HomeViewModel>(
+      () => _i104.HomeViewModel(authService: gh<_i620.AuthService>()),
+    );
+    return this;
+  }
+}
+
+class _$RegisterFlutterSecureStorageModule extends _i511.RegisterFlutterSecureStorageModule {}
--- mvvm_app_template/__brick__/{{app_name}}/lib/locator.dart
+++ mvvm_app_template/__brick__/{{app_name}}/lib/locator.dart
@@ -1,72 +1,11 @@
-// ignore_for_file: no-empty-block
-
-import 'dart:async';
-
 import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
 
-import 'features/auth/services/auth_service.dart';
-import 'features/core/services/logging_service.dart';
-import 'features/core/services/navigation/navigation_service.dart';
-import 'features/core/views/home/home_view_model.dart';
+import 'locator.config.dart';
 
-/// Locates dependency for DI
-GetIt get locate => Locator.instance();
+@InjectableInit(initializerName: 'init', preferRelativeImports: true, asExtension: true)
+Future<void> configureDependencies() async => GetIt.instance.init();
 
-/// Dependency Injection
-class Locator {
-  /// Instance of [GetIt]
-  static GetIt instance() => GetIt.instance;
-
-  /// Callback for performing a DI lookup
-  static T locate<T extends Object>() => instance().get<T>();
-
-  /// Set up of all DI registrations
-  static Future<void> setup() async {
-    final locator = instance()..registerFactory(LoggingService.new);
-
-    _registerStores(locator);
-    _registerAPIs(locator);
-    _registerViewModels(locator);
-    _registerLazySingletons();
-    _registerFactories();
-
-    await _registerDaos(locator);
-    await _registerServices(locator);
-
-    await _registerRepos(locator);
-    await _registerBlocs(locator);
-
-    _registerSingletons();
-  }
-
-  static void _registerAPIs(GetIt it) {}
-
-  static void _registerLazySingletons() {}
-
-  static void _registerSingletons() {}
-
-  static void _registerFactories() {}
-
-  static void _registerStores(GetIt it) {}
-
-  static Future<void> _registerBlocs(GetIt it) async {}
-
-  static Future<void> _registerDaos(GetIt it) async {}
-
-  static Future<void> _registerRepos(GetIt it) async {}
-
-  static Future<void> _registerServices(GetIt it) async {
-    it
-      ..registerLazySingleton(AuthService.new)
-      ..registerLazySingleton(NavigationService.new);
-  }
-
-  static void _registerViewModels(GetIt it) {
-    it.registerFactory(
-      () => HomeViewModel(
-        authService: AuthService.locate,
-        navigationService: NavigationService.locate,
-      ),
-    );
-  }
+abstract class Locator {
+  static T locate<T extends Object>() => GetIt.instance.get<T>();
 }
